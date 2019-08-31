331490e9e82fa31438ccb518200d22f224436477
Venipa <admin@venipa.net>
2019-08-31 14:50:24 +0200

    :sparkles: validator db presence, use db in validator

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 
+use Cochlea\Models\MyBBUsers;
 use Illuminate\Container\Container;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Translation;
@@ -7,17 +8,10 @@ use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Factory as ValidationFactory;
 use Illuminate\Database\Capsule\Manager as Capsule;
-
+use Illuminate\Validation\DatabasePresenceVerifier;
 
 $app = new Container();
 Facade::setFacadeApplication($app);
-$app->singleton('validator', function () use ($app) {
-    $filesystem = new Filesystem();
-    $fileLoader = new Translation\FileLoader($filesystem, __DIR__ . '/Lang');
-    $translator = new Translation\Translator($fileLoader, 'en');
-    $validator = new ValidationFactory($translator, $app);
-    return $validator;
-});
 $app->singleton('hash', function () use ($app) {
     return new Illuminate\Hashing\HashManager($app);
 });
@@ -26,6 +20,14 @@ $app->singleton('db', function() use ($app) {
     $capsule->setAsGlobal();
     $capsule->bootEloquent();
     return $capsule;
+});
+$app->singleton('validator', function () use ($app) {
+    $filesystem = new Filesystem();
+    $fileLoader = new Translation\FileLoader($filesystem, __DIR__ . '/Lang');
+    $translator = new Translation\Translator($fileLoader, 'en');
+    $validator = new ValidationFactory($translator, $app);
+    $validator->setPresenceVerifier(new DatabasePresenceVerifier(MyBBUsers::getConnectionResolver()));
+    return $validator;
 });
 class_alias(Illuminate\Support\Facades\Validator::class, 'Validator');
 class_alias(Illuminate\Support\Facades\Hash::class, 'Hash');
